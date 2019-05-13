58c1d5a4affffab8007d845dc311cfe9fe26f9bd
hao wang <ge_nah@hotmail.com>
2019-05-13 20:12:55 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,7 +7,12 @@ def second_challenge
   }
 
   #code your solution here!
-
-  
-
+  flat_arr = []
+  array = groceries.values 
+  array.each do |word|
+    word.each do |words|
+      flat_arr << words
+    end
+  end
+  flat_arr
 end
